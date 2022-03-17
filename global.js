88d3a644f808c3d5701742acb92e22a6821e7424
kc71486 <100687720+kc71486@users.noreply.github.com>
2022-03-18 00:59:33 +0800

    Add files via upload

---
+++ global.js
@@ -0,0 +1,23 @@
+var beers = {
+	id: [
+		document.getElementById('beer0'),
+		document.getElementById('beer1'),
+		document.getElementById('beer2'),
+		document.getElementById('beer3'),
+		document.getElementById('beer4'),
+		document.getElementById('beer5'),
+		document.getElementById('beer6')
+	],
+	style: [
+		document.getElementById('beer0').style,
+		document.getElementById('beer1').style,
+		document.getElementById('beer2').style,
+		document.getElementById('beer3').style,
+		document.getElementById('beer4').style,
+		document.getElementById('beer5').style,
+		document.getElementById('beer6').style
+	],
+	currentBeer: 6,
+	amount: [0, 0, 0, 0, 0, 0, 0],
+	prize: [100, 150, 200, 300, 250, 400, 100]
+}
